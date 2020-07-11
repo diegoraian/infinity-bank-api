d062c74f1df3f29a91731f786de697c1af6c806f
Diego Raian <diego.raian@gmail.com>
2020-07-10 22:29:02 -0300

    Cors

--- src/main/java/com/infinity/bank/api/config/AppConfig.java
+++ src/main/java/com/infinity/bank/api/config/AppConfig.java
@@ -1,9 +1,12 @@
 package com.infinity.bank.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.infinity.bank.api.interceptor.RequestInterceptor;
 @Configuration
@@ -15,5 +18,17 @@ public class AppConfig implements WebMvcConfigurer{
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
        registry.addInterceptor(productServiceInterceptor);
+    }
+    
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 }
